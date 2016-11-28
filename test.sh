a44c8e4dd7f0314a9eea0098119cf44bec272f0a
praveen <praveenkumar1232>
2016-11-28 13:36:28 +0530

    this is addition

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-echo "Enter two numbers"
-read  a  b
+echo -n "Enter the first number : "
+read num1
+echo -n "Enter the second number : "
+read num2
+sum=`expr $num1 + $num2`
+echo "sum of two value is $sum"
 
-sum = 'expr $a + $b'
-echo "The sum is = $sum"
